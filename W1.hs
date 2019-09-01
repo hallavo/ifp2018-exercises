a9499de1391e6baa799b129c8e108c8bb5c17063
Antti <antti.hallavo@aalto.fi>
2019-09-01 06:58:15 +0300

    w1, ex6: bug fix

--- W1.hs
+++ W1.hs
@@ -58,8 +58,8 @@ eeny x
 fizzbuzz :: Integer -> String
 fizzbuzz x
   | x `mod` 15 == 0 = "FizzBuzz"
-  | x `mod` 3  == 0 = "Buzz"
-  | x `mod` 5  == 0 = "Fizz"
+  | x `mod` 5  == 0 = "Buzz"
+  | x `mod` 3  == 0 = "Fizz"
   | otherwise       = ""
 
 
